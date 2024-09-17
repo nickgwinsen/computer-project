0d157f5c7fea190b329ea53d5159b98e438edf95
Nicholas Winsen <97001114+nickgwinsen@users.noreply.github.com>
2024-09-17 01:26:48 -0400

    nice

--- backend/main.py
+++ backend/main.py
@@ -9,12 +9,26 @@ app = FastAPI()
 
 rgapi = os.getenv("API_KEY")
 
+session = {}
 
-@app.get("/puuid")
-def get_puuid_with_name_and_tag():
-    ex_name = "longhorns"
-    ex_tag = "100"
-    url = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{ex_name}/{ex_tag}"
+BASE_URL = "https://americas.api.riotgames.com/"
+
+
+@app.get("/player/{game_name}/{game_tag}")
+def player(game_name: str, game_tag: str):
+    puuid_url = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{game_tag}"
+    headers = {"X-Riot-Token": rgapi}
+    puuid = get(puuid_url, headers=headers)
+    player_puuid = puuid.json()
+    url = f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{player_puuid['puuid']}/ids?start=0&count=20"
+    response = get(url, headers=headers)
+    content = response.json()
+    return content
+
+
+@app.get("/match/{match}")
+def get_match_by_match_id():
+    url = f"{BASE_URL}/riot/account/v1/active_shards/by-game/lor/by-puuid/xnbr8i3wA_LwJToCEYuvAkpAor7xvDE3FMuY0BKk8fkQ8sX5NvT6k_smVmhAQXjtNTpujew_IjyqWg"
     headers = {"X-Riot-Token": rgapi}
     response = get(url, headers=headers)
     content = response.json()
