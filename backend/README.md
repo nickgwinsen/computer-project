038738741106947e2daeaa938175eb1492a921ac
Nicholas Winsen <97001114+nickgwinsen@users.noreply.github.com>
2024-10-14 02:29:06 -0400

    semi-finish login and signup

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,6 @@
-Must create and add the current `pwd` to the virtual environment's site-packages folder.
+I was having issues with PYTHONPATH so I created and added a custom path to my virtual envrionment's site-packages folder, `custom.pth`, which contains the current `pwd`.
+
+- Struggled to figure out alembic migration on startup, seems to freeze oddly.
+- alembic commands:
+- make a db "commit": alembic revision --autogenerate -m "`<string>`"
+- update the db to latest commit: alembic upgrade head
--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Depends
-from sqlalchemy import text
+from fastapi import APIRouter, Depends, Response, status
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy import text, select
 from sqlalchemy.orm import Session
 
 from app.db.session import SessionDep, get_session
-from app.etc.crypt import hash_password
+from app.etc.crypt import hash_password, verify_password
 from app.models.user_model import *
 from app.schemas.user_schema import UserIn, UserOut
 
@@ -11,20 +12,27 @@ router = APIRouter()
 
 
 @router.post("/login")
-async def auth_login(db: SessionDep):
-    return {"db_version": db.exec(text("SELECT version();")).scalar()}
+async def auth_login(user: UserIn, db: SessionDep, response: Response):
+    user_hash = hash_password(user.password)
+    user_to_verify = select(Users).where(Users.email == user.email)
+    result = db.exec(user_to_verify).scalars().all()
+    result = jsonable_encoder(result)
+    if verify_password(user.password, result[0]["hashed_password"]):
+        response.status_code = status.HTTP_200_OK
+        return result
+    else:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {}
 
 
 @router.post("/signup")
-async def auth_login(user: UserIn, db: SessionDep):
+async def auth_signup(user: UserIn, db: SessionDep):
     user_hash = hash_password(user.password)
     user_to_add = Users(email=user.email, **user_hash)
     db.add(user_to_add)
     db.commit()
     db.refresh(user_to_add)
-    return user_to_add
+    return user_to_add.email
 
 
-@router.patch("/enable-two-factor")
-async def auth_login(db: Session = Depends(get_session)):
-    return {"db_version": db.execute(text("SELECT version();")).scalar()}
+# TODO: 2 factor auth, forgot password, logout, jwt functionality
